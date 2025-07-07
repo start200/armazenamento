c7952a0227907d390b8429efa7287840a4dbaf2f
start200 <almeidacaetanog@gmail.com>
2025-07-07 14:57:45 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,17 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  databaseURL: "https://SEU_PROJETO.firebaseio.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyBOXqar6tQqcUFVmhkdg4kOOOKZJCLHs7w",
+  authDomain: "authentication-cb7d2.firebaseapp.com",
+  databaseURL: "https://authentication-cb7d2-default-rtdb.firebaseio.com",
+  projectId: "authentication-cb7d2",
+  storageBucket: "authentication-cb7d2.firebasestorage.app",
+  messagingSenderId: "124652912298",
+  appId: "1:124652912298:web:9edc7caf0abd5888f59ad3",
+  measurementId: "G-EQ83C14TFX"
 };
 
 const app = initializeApp(firebaseConfig);
